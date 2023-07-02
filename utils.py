b71f3f00ac6abe9bfaf51d6ac04d2eb733fb243e
Sugam Garg <sgarg6@nlp-gpu-01.soe.ucsc.edu>
2023-07-02 11:32:07 -0700

    Added utility fn to read jsonl file

---
+++ utils.py
@@ -0,0 +1,11 @@
+import json
+
+
+def load_jsonl_file(filename):
+    with open(filename, "r") as json_file:
+        json_list = list(json_file)
+    data = []
+    for json_str in json_list:
+        result = json.loads(json_str)
+        data.append(result)
+    return data
