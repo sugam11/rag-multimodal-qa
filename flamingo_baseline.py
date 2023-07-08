8d733e848e3bfbda4eccb18743e278e802007463
Brian Farrell <farrelb6@tcd.ie>
2023-07-07 21:02:58 -0700

    webQA baseline placeholder code

--- flamingo_baseline.py
+++ flamingo_baseline.py
@@ -17,15 +17,31 @@ if data_set == "MMQA":
 	blank_image = Image.open('1x1_#00000000.png')
 
 	answers = {}
-	index = 0
 	for x in data_loader:
 		ques = x[0][0]
 		qid = x[1][0]
 		ans = baseline.generate_answer([blank_image], ques)
 		answers[qid] = ans
-		index = index + 1
 
-	print(answers)
+	with  open("MMQA_baseline_out.json", "w", encoding="utf-8") as f:
+		for k, v in answers.items():
+			line = {"qid": k, "answer": ' '.join(v.split())}
+			f.write(json.dumps(line) + "\n")
+	f.close()
+
+if data_set == "webQA":
+	data = dataset_mmqa.MMQAQuestionAnswerPairs("MMQA_dev.jsonl")
+	data_loader = DataLoader(data)
+	baseline = flamingo_model.FlamingoBaseline()
+	blank_image = Image.open('1x1_#00000000.png')
+
+	answers = {}
+	for x in data_loader:
+		ques = x[0][0]
+		qid = x[1][0]
+		ans = baseline.generate_answer([blank_image], ques)
+		answers[qid] = ans
+
 	with  open("MMQA_baseline_out.json", "w", encoding="utf-8") as f:
 		for k, v in answers.items():
 			line = {"qid": k, "answer": ' '.join(v.split())}
