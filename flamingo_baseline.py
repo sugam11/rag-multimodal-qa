7c72ac8c78b6863f2222e9b3532fdba37005cc77
Brian Farrell <farrelb6@tcd.ie>
2023-07-07 21:01:28 -0700

    baseline ready for testing

--- flamingo_baseline.py
+++ flamingo_baseline.py
@@ -1,16 +1,33 @@
-dev_data = dataset_mmqa.MMQAQuestionAnswerPairs("MMQA_dev.jsonl")
-loader = DataLoader(dev_data)
-baseline = FlamingoBaseline()
+import flamingo_model
+import dataset_mmqa
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import sys
+import optparse
+import json
 
-blank_image = Image.open('1x1_#00000000.png')
-demo_qs = ['Who is the current president of the United States?', 'What does the president do?', 'What is 10 + 4?',
-           'What colour is the sky']
+optparser = optparse.OptionParser()
+optparser.add_option("-d", "--data", dest="data_set", default='MMQA', help="MMQA or webQA data")
 
-answers = {}
-index = 0
-for x in demo_qs:
-    ans = baseline.generate_answer([blank_image], x)
-    answers[index] = ans
-    index = index + 1
+data_set = 'MMQA'
+if data_set == "MMQA":
+	data = dataset_mmqa.MMQAQuestionAnswerPairs("MMQA_dev.jsonl")
+	data_loader = DataLoader(data)
+	baseline = flamingo_model.FlamingoBaseline()
+	blank_image = Image.open('1x1_#00000000.png')
 
-print(answers)
+	answers = {}
+	index = 0
+	for x in data_loader:
+		ques = x[0][0]
+		qid = x[1][0]
+		ans = baseline.generate_answer([blank_image], ques)
+		answers[qid] = ans
+		index = index + 1
+
+	print(answers)
+	with  open("MMQA_baseline_out.json", "w", encoding="utf-8") as f:
+		for k, v in answers.items():
+			line = {"qid": k, "answer": ' '.join(v.split())}
+			f.write(json.dumps(line) + "\n")
+	f.close()
