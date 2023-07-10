954cb87dd4cb4ea6f045a0175944ee2c0a194f60
Brian Farrell <bfarrell@nlp-gpu-01.soe.ucsc.edu>
2023-07-09 21:08:11 -0700

    reformated baseline code

--- flamingo_baseline.py
+++ flamingo_baseline.py
@@ -15,42 +15,35 @@ optparser = optparse.OptionParser()
 optparser.add_option("-d", "--data", dest="data_set", default='MMQA', help="MMQA or webQA data")
 (opts, _) = optparser.parse_args()
 
+def generate_output(baseline, data):
+	blank_image = Image.open('1x1_#00000000.png')
+	answers = {}
+	for x in tqdm(data, position=0, leave=True):
+		ques = x[0][0]
+		qid = x[1][0]
+		ans = baseline.generate_answer([blank_image], ques)
+		answers[qid] = ans
+
+	path = opts.data_set +"_dev.json"
+	with open(path, "w") as outfile:
+    		json.dump(answers, outfile)
+
+	path = opts.data_set +"_dev_pretty.json"
+	with  open(path, "w", encoding="utf-8") as f:
+		for k, v in answers.items():
+			line = {"qid": k, "answer": ' '.join(v.split())}
+			f.write(json.dumps(line) + "\n")
 
 if opts.data_set == "MMQA":
 	data = dataset_mmqa.MMQAQuestionAnswerPairs("/data/users/sgarg6/capstone/multimodalqa/MMQA_dev.jsonl")
 	data_loader = DataLoader(data)
 	baseline = flamingo_model.FlamingoBaseline()
-	blank_image = Image.open('1x1_#00000000.png')
 
-	answers = {}
-	for x in tqdm(data_loader, position=0, leave=True):
-		ques = x[0][0]
-		qid = x[1][0]
-		ans = baseline.generate_answer([blank_image], ques)
-		answers[qid] = ans
-
-	with  open("MMQA_baseline_out_test.json", "w", encoding="utf-8") as f:
-		for k, v in answers.items():
-			line = {"qid": k, "answer": ' '.join(v.split())}
-			f.write(json.dumps(line) + "\n")
-	f.close()
-
+	generate_output(baseline, data_loader)
 
 if opts.data_set == "webQA":
 	data = webqa_dataset.WebQAQuestionAnswerPairs("/data/users/sgarg6/capstone/webqa/data/WebQA_test.json")
 	data_loader = DataLoader(data)
 	baseline = flamingo_model.FlamingoBaseline()
-	blank_image = Image.open('1x1_#00000000.png')
 
-	answers = {}
-	for x in tqdm(data_loader):
-		ques = x[0][0]
-		qid = x[1][0]
-		ans = baseline.generate_answer([blank_image], ques)
-		answers[qid] = ans
-
-	with  open("WebQA_baseline_out.json", "w", encoding="utf-8") as f:
-		for k, v in answers.items():
-			line = {"qid": k, "answer": ' '.join(v.split())}
-			f.write(json.dumps(line) + "\n")
-	f.close()
+	generate_output(baseline, data_loader)
