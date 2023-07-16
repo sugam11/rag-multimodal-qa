a76e9e333fb73d193884ae0a80debcce0c5121a1
Brian Farrell <bfarrell@nlp-gpu-01.soe.ucsc.edu>
2023-07-16 01:06:41 -0700

    new output files

--- experiment.py
+++ experiment.py
@@ -1,4 +1,5 @@
 import os
+os.environ['TRANSFORMERS_CACHE'] = '/data/users/bfarrell/models/'
 import flamingo_model
 from data_loaders import qa_dataset
 from torch.utils.data import Dataset, DataLoader
@@ -7,6 +8,7 @@ import sys
 import optparse
 import json
 from tqdm import tqdm
+import pandas as pd
 
 optparser = optparse.OptionParser()
 optparser.add_option(
@@ -25,35 +27,68 @@ def run_experiment(model, run_on=["MMQA", "WebQA"]):
             "MMQA", "val"
         )
         data_loader = DataLoader(data)
-        generate_output(3, model, data_loader)
+        generate_output_mmqa(3, model, data_loader)
 
     if "WebQA" in run_on:
         data = qa_dataset.get_dataset(
             "WebQA", "val"
         )
         data_loader = DataLoader(data)
-        generate_output(3, model, data_loader)
+        generate_output_webqa(3, model, data_loader)
 
 
-def generate_output(beams, baseline, data):
-    blank_image = Image.open("1x1_#00000000.png")
+def generate_output_mmqa(beams, baseline, data):
+    blank_image = Image.open("resources/1x1_#00000000.png")
     answers = {}
+    df = {'qid':[],
+               'Q':[],
+               'A':[]
+    }
     for x in tqdm(data, position=0, leave=True):
         ques = x[0][0]
-	question = "Q:" + ques + "A:"
         qid = x[1][0]
+        question = "Q:" + ques + "A:"
         ans = baseline.generate_answer(beams, [blank_image], question)
         answers[qid] = ans
+        df['qid'].append(qid)
+        df['Q'].append(ques)
+        df['A'].append(ans)
 
-    path = opts.data_set + "_base_dev.json"
-    with open(path, "w") as outfile:
+    df = pd.DataFrame(df)
+    df.to_csv("mmqa_base_dev.csv")
+
+    with open("mmqa_base_dev_.json", "w") as outfile:
         json.dump(answers, outfile)
+     
 
-    path = opts.data_set + "_base_dev_pretty.json"
-    with open(path, "w", encoding="utf-8") as f:
-        for k, v in answers.items():
-            line = {"qid": k, "answer": " ".join(v.split())}
-            f.write(json.dumps(line) + "\n")
+def generate_output_webqa(beams, baseline, data):
+    blank_image = Image.open("resources/1x1_#00000000.png")
+    answers = {}
+    df = {'qid':[],
+               'Qcate':[],
+               'Q':[],
+               'A':[],
+               'Keywords_A':[]
+               'Output_conf':[],
+               'Output':[]
+    }
+    for x in tqdm(data, position=0, leave=True):
+        ques = x[0][0]
+        qid = x[1][0]
+        question = "Q:" + ques + "A:"
+        ans = baseline.generate_answer(beams, [blank_image], question)
+        answers[qid] = ans
+        df['qid'].append(qid)
+        df['Qcate'].append(x[3][0])
+        df['Q'].append(ques)
+        df['A'].append(x[2][0])
+        df['Keywords_A'].append(x[4][0])
+        df['Output_conf'].append(1)
+        df['Output'].append(ans)
+
+    df = pd.DataFrame(df)
+    df.to_csv("webqa_base_dev.tsv", header=['#','Guid','Qcate','Q','A','Keywords_A','Output_conf','Output'])
+
 
 if __name__ == "__main__":
     model = flamingo_model.FlamingoModel("anas-awadalla/mpt-1b-redpajama-200b", "anas-awadalla/mpt-1b-redpajama-200b", 1)
