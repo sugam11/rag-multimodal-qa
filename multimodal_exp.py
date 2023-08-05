5dac7e8c5e565aaf3b80fbee41fbb7eeff73a3ee
Brian Farrell <bfarrell@nlp-gpu-02.soe.ucsc.edu>
2023-08-04 22:55:39 -0700

    code refactor

--- multimodal_exp.py
+++ multimodal_exp.py
@@ -19,68 +19,94 @@ def collate_fn(x):
     ques = [sample[0] for sample in x]
     qids = [sample[1] for sample in x]
     answers = [sample[2] for sample in x]
-    return ques, qids, answers
+    doc_ids = [sample[3] for sample in x]
+    return ques, qids, answers, doc_ids
 
-data_loader = DataLoader(data, batch_size = 8, collate_fn=collate_fn)
-#db = dataset_mmqa.MMQAKnowledgeBase("/data/users/sgarg6/capstone/multimodalqa/MMQA_texts.jsonl", "/data/users/sgarg6/capstone/multimodalqa/MMQA_images.jsonl", "/data/users/sgarg6/capstone/multimodalqa/final_dataset_images")
-#texts = db.get_all_texts()
-model = flamingo_model.FlamingoModel("anas-awadalla/mpt-1b-redpajama-200b", "anas-awadalla/mpt-1b-redpajama-200b", 1)
+data_loader = DataLoader(data, batch_size = 1, collate_fn=collate_fn)
+db = dataset_mmqa.MMQAKnowledgeBase("/data/users/sgarg6/capstone/multimodalqa/MMQA_texts.jsonl", "/data/users/sgarg6/capstone/multimodalqa/MMQA_images.jsonl", "/data/users/sgarg6/capstone/multimodalqa/final_dataset_images")
+model = flamingo_model.FlamingoModel("togethercomputer/RedPajama-INCITE-Instruct-3B-v1", "togethercomputer/RedPajama-INCITE-Instruct-3B-v1", 1)
 
-blank_image = Image.open("resources/1x1_#00000000.png")
-answers = {}
-df = {'qid':[],
-      'Q':[],
-      'A':[],
-      #'Gold_A':[],
-      #'docs':[]
-}
-for x in tqdm(data_loader, position=0, leave=True):
-    ques = x[0]
-    qids = x[1]
-    #golds = x[2]
-    #print(golds)
 
-    prompts = []
-    imgs = []
-    for i, q in enumerate(ques):
-         #docs = x[3]
-         #doc_ids = []
-         #for doc in docs:
-             #doc_ids.append(doc[i])
-         #texts = db.get_all_texts()
-         #docs_retrieved = []
-         #for text in texts:
-             #if text['id'] in doc_ids:
-                #docs_retrieved.append(text['text'])
+def clean_output(output):
+    output = re.sub('<image>[^>]+A:', '', output)
+    output = re.sub('<[^>]+>', '', output)
 
-         #context = "".join([doc for doc in docs_retrieved[0:2]])   
-         context = ""
-         df['Q'].append(q)
-         prompt = "<image> Passage: " + context + "\nAnswer the following question and output only the answer. \nQ " + q + "\nA: " 
-         prompts.append(prompt)
-         imgs.append(model.process_imgs([blank_image]))
+    return output
 
-    prompts = model.process_text(prompts)
-    imgs = torch.stack(imgs, dim=0)
-    out = model.generate_answer(8, imgs, prompts)
-    for i, ans in enumerate(out):
-        ans = re.sub('<image>[^>]+A:', '', ans)
-        ans = re.sub('<[^>]+>', '', ans)
-        #print("Answer: ", ans, " end of answer")
 
-        qid = qids[i]
-        #gold = golds[i]
-        answers[qid] = ans
-        df['qid'].append(qid)
-        df['A'].append(ans)
-        #df['Gold_A'].append(gold)
-        #df['docs'].append(docs_retrieved)
+def prompt_builder(q, docs, db, gold):
+    doc_ids = []
+    for doc in docs:
+        doc_ids.append(doc)
+    texts = db.get_all_texts()
+    images = db.get_all_images()
 
-#df = pd.DataFrame(df)
-#path = "gold_mmqa_base_dev.csv"
-#df.to_csv(path)
+    text_docs = []
+    for text in texts:
+        if text['id'] in doc_ids:
+           text_docs.append(text['text'])
 
-#path = "gold_mmqa_base_dev.json"
-#with open(path, "w") as outfile:
-     #json.dump(answers, outfile)
+    img_docs = []
+    for img in images:
+        if img['id'] in doc_ids:
+           img_docs.append(img["path"])
+             
+    context = ""
+    if text_docs:
+       context = "".join([doc for doc in text_docs])  
 
+    if img_docs:
+        imgs = [db.get_image(img) for img in img_docs]
+        img_list.append(model.process_imgs(imgs))
+    else:
+        img_list.append(model.process_imgs([blank_image]))
+
+    df['Q'].append(q)
+    df['Gold_A'].append(gold)
+    df['docs'].append(doc_ids)
+    prompt = "<image> Passage: " + context + "\nAnswer the following question and output only the answer. \nQ " + q + "\nA: " 
+    return prompt
+
+
+def evaluate(data_loader,db, model, texts):
+    blank_image = Image.open("resources/1x1_#00000000.png")
+    answers = {}
+    df = {'qid':[],
+          'Q':[],
+          'A':[],
+          'Gold_A':[],
+          'docs':[]
+    }
+    ctr = 0
+    for x in tqdm(data_loader, position=0, leave=True):
+        ques = x[0]
+        qids = x[1]
+        golds = x[2]
+        docs = x[3]   
+        prompts = []
+        img_list = []
+        for i, q in enumerate(ques):
+             gold = golds[i]
+             doc_ids = docs[i]
+             prompt = prompt_builder(q, doc_ids, db, gold)
+             prompts.append(prompt)
+
+        prompts = model.process_text(prompts)
+        imgs = torch.stack(img_list, dim=0)
+        out = model.generate_answer(8, imgs, prompts)
+        for i, ans in enumerate(out):
+             ans = clean_output(ans)
+             qid = qids[i]
+             answers[qid] = ans
+             df['qid'].append(qid)
+             df['A'].append(ans)
+
+    df = pd.DataFrame(df)
+    path = "gold_mmqa_base_dev_both.csv"
+    df.to_csv(path)
+
+    path = "gold_mmqa_base_dev_both.json"
+    with open(path, "w") as outfile:
+         json.dump(answers, outfile)
+
+evaluate(data_loader, db, model, texts)
