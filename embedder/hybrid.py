d3a699ff2028cfab1d76568a79dc7250d71a8215
Sugam Garg <sgarg6@nlp-gpu-02.soe.ucsc.edu>
2023-08-14 13:14:48 -0700

    Added change for hybrid text retriever

---
+++ embedder/hybrid.py
@@ -0,0 +1,32 @@
+import torch
+from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer
+from sentence_transformers import SentenceTransformer, util
+
+class HybridEmbedder:
+    def __init__(self, model_ID, device):
+        # Save the model to device
+        self.image_model = CLIPModel.from_pretrained(model_ID).to(device)
+        # Get the processor
+        self.image_processor = CLIPProcessor.from_pretrained(model_ID)
+        # Get the tokenizer
+        self.image_tokenizer = CLIPTokenizer.from_pretrained(model_ID)
+        self.text_embedder = SentenceTransformer("all-mpnet-base-v2")
+        self.pretty_name = "HYBRID"
+        self.device = device
+    
+    def get_name (self,):
+        return self.pretty_name
+    
+    def get_text_embedding(self, text):
+        text_embeddings = self.text_embedder.encode(text, convert_to_tensor=True)
+        embedding_as_np = text_embeddings.cpu().detach().numpy()
+        return embedding_as_np
+
+    def get_img_embedding(self, img):
+        image = self.image_processor(text=None, images=img, return_tensors="pt")[
+            "pixel_values"
+        ].to(self.device)
+        embedding = self.image_model.get_image_features(image)
+        # convert the embeddings to numpy array
+        embedding_as_np = embedding.cpu().detach().numpy()
+        return embedding_as_np
